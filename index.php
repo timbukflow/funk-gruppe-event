af103930f4a7c17572d7fa2a6f2521520dcbdf97
Ivo Schwizer <ivoschwizer@gmail.com>
2023-07-08 12:07:53 +0200

    deactivate form and information save for next event

--- index.php
+++ index.php
@@ -15,12 +15,12 @@
         <div class="event-title">
           <h1 >Funk Grill-Plausch <span>Versichereranlass mit Fussball-Special</span></h1>
           <h2>Donnerstag 22. Juni 2023</span></h2>
-          <button class="homebutton"> <a class="goto" href="#anmeldung">zur Anmeldung</a> </button>
+          <!-- <button class="homebutton"> <a class="goto" href="#anmeldung">zur Anmeldung</a> </button> -->
           <div class="grill"><img src="./img/funk-grill.svg" alt="funk-grill"></div>
           <div class="funk-logo"><img src="./img/funk-logo.svg" alt="funk-logo"></div>
         </div>
     </header>
-    <section>
+    <!-- <section>
       <article class="intro">
         <h1>Geschätzte Geschäftspartner</h1>
         <p>
@@ -86,51 +86,8 @@
       <div id="anmeldung" class="containerform">
             <h2>Anmeldung</h2>
             <p>Anmeldeschluss ist der 31. März 2023. <br> Die Teilnehmerzahl ist beschränkt. Die Anmeldungen werden nach Eingang berücksichtigt.</p>
-            <?php include('form.php'); ?>
-
-            <form id="contact" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" novalidate>
-              <fieldset class="checkbox">
-                <div>
-                    <input class="radio" type="radio" id="checkbox1" name="checkbox[]" value="Ja, ich nehme gerne teil" tabindex="1" <?= (is_array($checkbox) && in_array("Ja, ich nehme gerne teil", $checkbox)) ? "checked" : "" ?>>
-                    <label for="checkbox1">Ja, ich nehme gerne teil</label>
-                </div>
-                <div>
-                  <input class="radio" type="radio" id="checkbox2" name="checkbox[]" value="Ja, ich nehme gerne teil und mache am Töggeliturnier mit" tabindex="2" <?= (is_array($checkbox) && in_array("Ja, ich nehme gerne teil und mache am Töggeliturnier mit", $checkbox)) ? "checked" : "" ?>>
-                  <label for="checkbox2">Ja, ich nehme gerne teil und mache am Töggeliturnier mit</label>
-                </div>
-                <div>
-                  <input class="radio" type="radio" id="checkbox3" name="checkbox[]" value="Leider bin ich verhindert" tabindex="3"<?= (is_array($checkbox) && in_array("Leider bin ich verhindert", $checkbox)) ? "checked" : "" ?>>
-                  <label for="checkbox3">Leider bin ich verhindert</label>
-                </div>
-                <span class="error"><?= isset($errors["checkbox"]) ? $errors["checkbox"] : $checkbox_error ?></span>
-              </fieldset>
-              <fieldset>
-                  <input placeholder="Vorname&#42;" type="text" name="vorname" value="<?= $vorname ?>" tabindex="4" autofocus>
-                  <span class="error"><?= isset($errors["vorname"]) ? $errors["vorname"] : $vorname_error ?></span>
-              </fieldset>
-              <fieldset>
-                  <input placeholder="Name&#42;" type="text" name="name" value="<?= $name ?>" tabindex="5">
-                  <span class="error"><?= isset($errors["name"]) ? $errors["name"] : $name_error ?></span>
-              </fieldset>
-              <fieldset>
-                  <input placeholder="Firma&#42;" type="text" name="firma" value="<?= $firma ?>" tabindex="6">
-                  <span class="error"><?= isset($errors["firma"]) ? $errors["firma"] : $firma_error ?></span>
-              </fieldset>
-              <fieldset>
-                  <input placeholder="Email&#42;" type="text" name="email" value="<?= $email ?>" tabindex="7">
-                  <span class="error"><?= isset($errors["email"]) ? $errors["email"] : $email_error ?></span>
-              </fieldset>
-              <fieldset>
-              <textarea placeholder="Mitteilung" name="mitteilung" tabindex="8" rows="5"><?= $mitteilung ?></textarea>
-                <span class="error"><?= isset($errors["mitteilung"]) ? $errors["mitteilung"] : $mitteilung_error ?></span>
-              </fieldset>
-              <fieldset>
-                  <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Anfrage senden</button>
-              </fieldset>
-              <div class="success"><?= $success; ?></div>    
-            </form>
       </div> 
-    </section>
+    </section> -->
 
     <?php require_once 'footer.php'; ?>
     <?php require_once 'script.php'; ?>
