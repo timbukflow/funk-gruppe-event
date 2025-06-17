9ff3bd100082280f98cc612bbe24a117f534f7ca
Ivo Schwizer <ivoschwizer@gmail.com>
2025-06-17 11:22:31 +0200

    update

--- index.php
+++ index.php
@@ -11,13 +11,13 @@ Version 2.0 - 27.03.2025
 <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# place: http://ogp.me/ns/place#">
   
     <meta charset="UTF-8">
-    <title>Funk Gruppe Event - Einladung zum Grillabend</title>
+    <title>Funk Gruppe Event</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <meta name="description" content="Funk Versichereranlass 2025 - Als Dankeschön für unsere erfolgreiche Zusammenarbeit laden wir Sie herzlich zu einem entspannten Grillabend ein. Geniessen Sie mit uns kulinarische Köstlichkeiten vom Grill, gute Gespräche und spannende Fussballmomente bei unserem beliebten Fussball-Special!">
-    <meta name="keywords" content="Funk Gruppe Event, Einladung zum Grillabend">
+    <meta name="description" content="Funk Gruppe Event">
+    <meta name="keywords" content="Funk Gruppe Event">
     <meta name="author" content="Funk Insurance Brokers AG">
-    <link rel="canonical" href="https://versichereranlass2025.funk-gruppe-event.ch/" />
+    <link rel="canonical" href="https://funk-gruppe-event.ch/" />
 
     <meta name="robots" content="index, follow" /> 
     <meta http-equiv="cache-control" content="public, max-age=3600" />
@@ -30,17 +30,17 @@ Version 2.0 - 27.03.2025
     <meta name="apple-mobile-web-app-title" content="Funk Gruppe" />
     <link rel="manifest" href="/site.webmanifest" />
 
-    <meta property="og:title" content="Funk Gruppe - Einladung zum Grillabend">
-    <meta property="og:description" content="Funk Versichereranlass 2025 - Als Dankeschön für unsere erfolgreiche Zusammenarbeit laden wir Sie herzlich zu einem entspannten Grillabend ein. Geniessen Sie mit uns kulinarische Köstlichkeiten vom Grill, gute Gespräche und spannende Fussballmomente bei unserem beliebten Fussball-Special!">
+    <meta property="og:title" content="Funk Gruppe">
+    <meta property="og:description" content="Funk Gruppe Event">
     <meta property="og:type" content="website">
-    <meta property="og:url" content="https://versichereranlass2025.funk-gruppe-event.ch/">
-    <meta property="og:image" content="https://versichereranlass2025.funk-gruppe-event.ch/img/og-image.jpg">
+    <meta property="og:url" content="https://funk-gruppe-event.ch/">
+    <meta property="og:image" content="https://funk-gruppe-event.ch/img/og-image.jpg">
     <meta property="og:locale" content="de_CH">
 
     <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:title" content="Funk Gruppe - Einladung zum Grillabend">
-    <meta name="twitter:description" content="Funk Versichereranlass 2025 - Als Dankeschön für unsere erfolgreiche Zusammenarbeit laden wir Sie herzlich zu einem entspannten Grillabend ein. Geniessen Sie mit uns kulinarische Köstlichkeiten vom Grill, gute Gespräche und spannende Fussballmomente bei unserem beliebten Fussball-Special!">
-    <meta name="twitter:image" content="https://versichereranlass2025.funk-gruppe-event.ch/img/twitter-image.jpg">
+    <meta name="twitter:title" content="Funk Gruppe">
+    <meta name="twitter:description" content="Funk Gruppe Event">
+    <meta name="twitter:image" content="https://funk-gruppe-event.ch/img/twitter-image.jpg">
 
     <meta name="format-detection" content="telephone=yes">
     <meta property="business:contact_data:street_address" content="Hagenholzstrasse 56">
@@ -58,90 +58,11 @@ Version 2.0 - 27.03.2025
 
 <body>
     <header>
-        <!-- <div class="asheader"><h3>Anmeldeschluss ist der 12. Mai 2025.</h3> </div> -->
         <div class="event-title">
-          <h1>Einladung zum Grillabend mit Töggeliturnier</span></h1>
-          <h2>Gemeinsam geniessen & mitfiebern!</span></h2>
-          <!-- <button class="homebutton"> <a class="goto" href="#anmeldung">zur Anmeldung</a> </button> -->
-          <div class="grill"><img src="./img/funk-grill.svg" alt="funk-grill"></div>
+          <h1>Momentan steht kein Event an – aber der nächste Funk-Moment kommt bestimmt!</span></h1>
           <div class="funk-logo"><img src="./img/funk-logo.svg" alt="funk-logo"></div>
         </div>
     </header>
-    <section class="no-form">
-      <article class="intro">
-        <h1>Geschätzte Geschäftspartner</h1>
-        <p>
-          <span>
-            Als Dankeschön für unsere erfolgreiche Zusammenarbeit laden wir Sie herzlich zu einem entspannten Grillabend ein. Geniessen Sie mit uns kulinarische Köstlichkeiten vom Grill, gute Gespräche und spannende Fussballmomente bei unserem beliebten <strong>Fussball-Special!</strong>
-          </span>
-          <span>
-            📅 Datum: 12. Juni 2025 <br>
-            📍 Ort: Funk Zürich, Hagenholzstrasse 56, 8050 Zürich 
-          </span>
-          <span>
-          Wir freuen uns, Sie an diesem Anlass zu begrüssen und erwarten gerne Ihre Anmeldung über das Anmeldeformular. Die Plätze sind begrenzt und werden nach Eingang berücksichtigt. 
-          </span>
-          <span>
-            Herzliche Grüsse <br>
-            Ihre Funkianer
-          </span>
-        </p>
-      </article>
-
-      <article>
-        <div class="acccont">
-          <div class="acctitle">
-            <h2>Programm</h2>
-            <div class="pmcontainer">
-              <div class="plus"></div>
-              <div class="minus"></div>
-            </div>
-          </div>
-          <div class="acclist">
-            <div class="acclistc">
-              <p>17.00 Uhr</p>
-              <p>Eintreffen der Gäste und Apéro</p>
-              <p>17.45 Uhr</p>
-              <p>Begrüssung</p>
-              <p>18.00 Uhr</p>
-              <p>Grill-Plausch</p>
-              <p>19.15 Uhr</p>
-              <p>Start Töggeliturnier</p>
-              <p>21.15 Uhr</p>
-              <p>Ende Töggeliturnier und Rangverkündung</p>
-              <p class="noline">Bis 23.00 Uhr</p>
-              <p class="noline">Ausklang</p>
-            </div>
-          </div>
-        </div>
-        <div class="acccont no-form">
-          <div class="acctitle"> 
-            <h2>Anreise</h2>
-            <div class="pmcontainer">
-              <div class="plus"></div>
-              <div class="minus"></div>
-            </div>
-          </div>
-          <div class="acclist">
-            <div class="acclistc">
-              <p class="green">Adresse</p>
-              <p class="green">Hagenholzstrasse 56, 8050 Zürich</p>
-              <p>zu Fuss</p>
-              <p>ab Bahnhof Oerlikon Fussweg ca. 10 Minuten</p>
-              <p>mit ÖV</p>
-              <p>ab Bahnhof Oerlikon mit Bus Nr 781 bis Haltestelle &laquo;Hagenholz&raquo; oder Tram 11 bis Haltestelle &laquo;Leutschenbach&raquo; und kurzer Fussweg</p>
-              <p>mit PW</p>
-              <p>folgen Sie dem Zeichen &laquo;Messe Zürich&raquo; und parkieren Sie im Parkhaus Messe Zürich AG</p>
-              <p class="noline gitem02">Kontaktperson bei Fragen:</p>
-              <p class="noline gitem02">
-                Jennifer Höltschi <br>
-                <a class="gitem02" href="mailto: jennifer.hoeltschi@funk-gruppe.ch" target="_top"> jennifer.hoeltschi@funk-gruppe.ch</a>
-              </p>
-            </div>
-          </div>
-        </div>
-      </article>
-    </section>
 
     <?php require_once 'footer.php'; ?>
     <?php require_once 'script.php'; ?>
